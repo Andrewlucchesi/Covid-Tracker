ee0185fca6555c0145bf0ef2e06a3a919b2a0cd0
Clement Decker <clement.decker18@gmail.com>
2020-05-06 11:25:05 -0700

    Started implementation for table

--- covid-tracker/src/Components/Links.js
+++ covid-tracker/src/Components/Links.js
@@ -1,13 +1,48 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 
-class List extends React.Component {
+class List extends Component {
+    
+    constructor(props)
+    {
+        super(props)
+        this.state = {
+            links: [
+                { link: "report a diagnosis" },
+                { link: "view recent cases" }
+            ]
+        }
+    }
+
+    renderTableData() {
+        return this.state.links.map((option, index) => {
+            const { link } = option
+            return (
+                <tr key={link}>
+                    <td>{link}</td>
+                </tr>
+            )
+            
+        })
+    }
+    
     render() {
         return (
             <div>
-                <p>
-                    Please choose a link from list below
-                </p>
+                <h1>What would you like to do?</h1>
+                <table id = 'options' >
+                    <tbody style={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        fontWeight: 'bold',
+                        fontFamily: 'Times New Roman',
+                        lineHeight: 10,
+                        fontSize: 50
+                        }}>
+                        {this.renderTableData()}
+                    </tbody>
+                </table>
             </div>
         )
     }
