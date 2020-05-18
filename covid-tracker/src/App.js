aad0a3992153576ca83b9ae4608dc33ece4cac81
Clement Decker <clement.decker18@gmail.com>
2020-05-17 22:24:25 -0700

    Added API Functionality

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { fetchData, fetchCountryData, fetchUSACountyData } from './api'
 import Navbar from './components/Navbar'
 import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
@@ -6,15 +7,33 @@ import Cases from './components/Cases'
 import Info from './components/Info'
 import Report from './components/Report'
 
+
 class App extends Component{
+  state = {
+    data: {},
+    CountryData: {},
+    countyData: {},
+  }
+
+  async componentDidMount() {
+    
+    const CountryData = await fetchCountryData();
+    this.setState({ CountryData: CountryData});
+    
+  }
   render () {  
+    
+    console.log(this.state.countyData.message);
     return (
       <BrowserRouter>
         <div className="App">
           <Navbar />
             <Route exact path='/' component={Home} />
             <Route path='/cases' component={Cases} />
-            <Route path='/info' component={Info} />
+            <Route 
+              path='/info' 
+              render={(props) => <Info {...props} CountryData={this.state.CountryData} />}
+            />
             <Route path='/report' component={Report} />
         </div>
       </BrowserRouter>
