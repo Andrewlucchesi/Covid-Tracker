429a4fe9c35f8c3d35ac725dccc4a67d92af9409
Andrewlucchesi <andrew.lucchesi13@gmail.com>
2020-05-20 18:28:41 -0700

    Sync firebase data with report page

--- covid-tracker/src/store/reducers/rootReducer.js
+++ covid-tracker/src/store/reducers/rootReducer.js
@@ -1,8 +1,10 @@
 import reportReducer from './reportReducer'
 import { combineReducers } from 'redux'
+import { firestoreReducer } from 'redux-firestore'
 
 const rootReducer = combineReducers({
-    report: reportReducer
+    report: reportReducer,
+    firestore: firestoreReducer
 });
 
 export default rootReducer
