ebb69a0af096547f6a60f1d0919f51ac06d1b5d9
Clement Decker <clement.decker18@gmail.com>
2020-05-21 17:16:36 -0700

    Added Info Windows with Covid Stats

--- covid-tracker/src/Maps/Map.js
+++ covid-tracker/src/Maps/Map.js
@@ -1,48 +1,118 @@
 import React, {Component, useState} from 'react';
 import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react';
 import countryLoc from './Country'
-// import {fetchCountryData} from './api'
+// import {fetchCountryData} from '../api'
 
 const mapStyles = {
   width: '80%',
   height: '70%',
 };
 
+
+
+
 export class MapContainer extends Component {
   constructor(props) {
     super(props);
-    this.state = { countryLoc, isOpen: false }
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+    this.onClose = this.onClose.bind(this);
+    this.state = { countryLoc, 
+                  isOpen: false,
+                  showingInfoWindow: false,
+                  activeMarker: {},
+                  selectedPlace: {},
+                  Disabled: true,
+                  countryData: props.CountryData,
+                };
+    console.log(this.state.countryData);
   }
 
-  // handleToggleOpen = () => {
-  //   this.setState({
-  //     isOpen: true
-  //   });
-  // }
+  componentDidMount() {
+    this.setState({
+      Disabled: false
+    })
+  }
+  getInitialState() {
+    return {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: "",
+    }
+  }
 
-  // handleToggleClose = () => {
-  //   this.setState({
-  //       isOpen: false
-  //   });
-  // }
+  onMarkerClick(props, marker, e) {
+    
+    for(const country of this.state.countryData)
+    {
+      if(country.CountryCode == props.country)
+      {
+        console.log(country.Country);
+        let text =  "Total Cases: " + country.TotalConfirmed + "\n Total Deaths: " + country.TotalDeaths;
+
+        this.setState({
+          
+          selectedPlace: text,
+          activeMarker: marker,
+          showingInfoWindow: true
+        });
+        return;
+      }
+    }
+    
+  }
+
+  onClose = props => {
+    if(this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  }
 
   displayMarkers = () => {
     return this.state.countryLoc.map((location, index) => {
-      return <Marker 
-        key={index} 
-        id={index} 
-        position={{
-          lat: location.latitude,
-          lng: location.longitude
-        }}
-        onClick={() => console.log("You clicked me!")} 
-        // onClick={() => this.handleToggleOpen(index)}
-      >
-      </Marker>
+      return [
+        <Marker 
+          key={index} 
+          id={index} 
+          position={{
+            lat: location.latitude,
+            lng: location.longitude
+          }}
+          onClick={this.onMarkerClick}
+          
+          country={location.countryCode}
+          
+        // onClick={() => console.log("Hello")}
+        >
+          
+         </Marker>,
+        <InfoWindow
+          onCloseClick={this.onClose}
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          
+          >
+            <div>
+              <h4>{this.state.selectedPlace}</h4>
+            </div>
+          
+          </InfoWindow>  
+      ]
+      
+      
+
     })
   }
 
+  
+
   render() {
+    if((this.state.Disabled))
+      {
+        return null;
+      }
     return (
         <Map
           google={this.props.google}
@@ -51,13 +121,7 @@ export class MapContainer extends Component {
           initialCenter={{ lat: 39.8283, lng: -98.5795}}
         >
           {this.displayMarkers()}
-          {/* <Marker
-            lat={39.8283}
-            lng={-98.5795}
-            name="My Marker"
-            color="red"
-          /> */}
-        </Map>
+        </Map> 
     );
   }
 }
