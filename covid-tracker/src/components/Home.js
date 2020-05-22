6046b4b73a9aa5e1ea1dad09a60b77d6233f8399
Clement Decker <clement.decker18@gmail.com>
2020-05-21 17:18:46 -0700

    Passed Country Data to Map.js

--- covid-tracker/src/components/Home.js
+++ covid-tracker/src/components/Home.js
@@ -1,10 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-// import Select from 'react-select'
-// import countryList from 'react-select-country-list'
 import Map from '../Maps/Map'
 
-  
 const mapStyles = {
   width: '80%',
   height: '50%'
@@ -12,31 +9,21 @@ const mapStyles = {
 
 // export class MapContainer extends Component {
 class Home extends Component {
-  state = {
-    content: ''
-  };
-
   constructor(props) {
-    super(props)
-    // this.options = countryList().getData()
+    super(props);
     this.state = {
       options: this.options,
       value: null,
+      CountryData: props.CountryData,
     }
   }  
 
   render () {
+    
     return (
       <div className="container">
-        {/* <label>
-          <Select
-            options={this.state.options}
-            value={this.state.value}
-            onChange={this.changeHandler}
-          />
-        </label> */}
         <Link to="/" style={{ marginRight: 10 }}></Link>
-        <Map />
+        <Map CountryData={this.state.CountryData}/>
       </div>
     )
   }
