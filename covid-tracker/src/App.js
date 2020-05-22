18bd47a07831a53336da181c2ffe99f861af9da2
Clement Decker <clement.decker18@gmail.com>
2020-05-21 17:14:57 -0700

    Passed CountryData as prop to Home Component

--- covid-tracker/src/App.js
+++ covid-tracker/src/App.js
@@ -1,23 +1,30 @@
 import React, { Component } from 'react';
-import { fetchData, fetchCountryData, fetchUSACountyData } from './api'
-import { BrowserRouter, Route } from 'react-router-dom'
+import {  fetchCountryData, fetchUSACountyData } from './api'
 import Navbar from './components/Navbar'
+import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
 import Cases from './components/Cases'
 import Info from './components/Info'
 import Report from './components/Report'
 
+
 class App extends Component{
+  
+    
     state = {
       CountryData: [],
       CountyData: [],
       Disabled: true,
   }
 
+  
+
   async componentDidMount() {
+    
     const CountryData = await fetchCountryData();
     const CountyData = await fetchUSACountyData();
-    console.log(CountyData);
+    
+    
     this.setState({ CountryData: CountryData,
                     Disabled: false,
                     CountyData: CountyData});
@@ -31,15 +38,25 @@ class App extends Component{
       }
     
     return (
+      
       <BrowserRouter >
         <div className="App">
           <Navbar />
-            <Route exact path='/' component={Home} />
+          {/* <Route 
+              exact path='/'
+              render={props => <Home CountryData={this.state.CountryData} />}
+              /> */}
+            <Route 
+              exact path='/'
+              render={(props) => <Home {...props} CountryData={this.state.CountryData} />} 
+              />
+            
             <Route path='/cases' component={Cases} />
             <Route 
               path='/info' 
               render={(props) => <Info {...props} CountryData={this.state.CountryData} CountyData={this.state.CountyData} />}
             />
+
             <Route path='/report' component={Report} />
         </div>
       </BrowserRouter>
@@ -47,4 +64,4 @@ class App extends Component{
   }
 }
 
-export default App;
+export default App; 
