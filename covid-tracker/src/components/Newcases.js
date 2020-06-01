b909abf46fc87a8ab8cb14ec10264e645c07f4a4
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-01 14:40:47 -0700

    Update Newcases.js

--- covid-tracker/src/components/Newcases.js
+++ covid-tracker/src/components/Newcases.js
@@ -10,25 +10,19 @@ class Newcases extends Component {
     let CountryStats = {};
     
 
-    for(const entry of props.Data)
-    {
-      if('Country_Code' in entry)
-      {
-        
+    for(const entry of props.Data){
+      if('Country_Code' in entry){
         let country = entry.Country_Code;
-        if(country in CountryStats)
-        {
+        if(country in CountryStats){
           CountryStats[country] = CountryStats[country] + 1;
         }
-        else
-        {
+        else{
           CountryStats[country] = 1;
         }
       }
     }
 
-    for(const country in CountryStats)
-    {
+    for(const country in CountryStats){
       let NewDataEntry = {};
       NewDataEntry.CountryCode = country;
       NewDataEntry.TotalConfirmed = CountryStats[country];
@@ -46,108 +40,97 @@ class Newcases extends Component {
     }
   }
 
-handleChange =(e) => {
+  handleChange =(e) => {
     this.setState({
-        value: e.target.value
-     })
-}
-handleSubmit =(e) => {
+      value: e.target.value
+    })
+  }
+
+  handleSubmit =(e) => {
     this.setState({
-        showText: true
+      showText: true
     })
     e.preventDefault();
-}
+  }
 
-displayCityCases = () => {
-  if(!this.state.showText)
-  {
-    return(
-      <p></p>
+  displayCityCases = () => {
+
+    if(!this.state.showText){
+      return( 
+        <p></p> 
+      )
+    }
+
+    let table = [];
+    let cityFound = false;
+    let caseCount = 0;
+
+    for(const entry of this.state.cases){
+      let city = entry.city;
+      if(((city).toLowerCase()) === (this.state.value).toLowerCase()){
+        cityFound = true;
+        let children = [];
+        let key = entry.id;
+
+        children.push(<td>{entry.city}</td>);
+        children.push(<td>{entry.zip}</td>);
+        table.push(<tr key={key}>{children}</tr>);
+        caseCount++;
+      }
+
+      if(caseCount >= 10){
+          break;
+      }
+    }
+
+    if(!cityFound){
+      return(
+        <p>No recently reported cases in that city!</p>
+      )
+    }
+    else{
+      return table;
+    }
+  }
+
+  onClickMap = (e) => {
+    this.setState({
+      showMap: !this.state.showMap,
+      showTable: false
+    })
+  }
+
+  onClickTable = (e) => {
+    this.setState({
+      showTable: !this.state.showTable,
+      showMap: false
+    })
+  }
+
+  displayTable = () => {
+    return (
+      <div className="container">
+        <h4 className="center">Search for Recently Reported Cases</h4>
+          <p>Limited to last 10 reported cases</p>
+          <form onSubmit={ this.handleSubmit }>  
+            <div className="input-field"> 
+              <label htmlFor="city">Search by city</label>
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+            </div>
+            <input type="submit" value="Search for Recently Reported Cases" />
+          </form>
+          <table>{this.displayCityCases()}</table>
+      </div>
     )
   }
-
-  let table = [];
-  let cityFound = false;
-  let caseCount = 0;
-  for(const entry of this.state.cases)
-  {
-    let city = entry.city;
-    if(((city).toLowerCase()) === (this.state.value).toLowerCase())
-    {
-            cityFound = true;
-            let children = [];
-            let key = entry.id
-            children.push(<td>{entry.city}</td>);
-            children.push(<td>{entry.zip}</td>);
-            table.push(<tr key={key}>{children}</tr>);
-            caseCount++;
-    }
-
-    if(caseCount >= 10)
-      {
-        break;
-      }
-  }
-
-  if(!cityFound)
-    {
-        return(
-            <p>No recently reported cases in that city!</p>
-        )
-    }
-    else
-    {
-        return table;
-    }
-}
-
-onClickMap = (e) => {
-  this.setState({
-    showMap: !this.state.showMap,
-    showTable: false
-  })
-}
-
-onClickTable = (e) => {
-  this.setState({
-    showTable: !this.state.showTable,
-    showMap: false
-  })
-}
-
-displayTable = () => {
-  return (
-    <div className="container">
-         <h4 className="center">Search for Recently Reported Cases</h4>
-           <p>Limited to last 10 reported cases</p>
-                 <form onSubmit={ this.handleSubmit }>  
-                     <div className="input-field"> 
-                     <label htmlFor="city">Search by city</label>
-                         <input type="text" value={this.state.value} onChange={this.handleChange} />
-                     </div>
-                     <input type="submit" value="Search for Recently Reported Cases" />
-                   
-                 </form>
-                 <table>
-                     {this.displayCityCases()}
-                 </table>
-       </div>
-  )
-}
   render () {
-    // let isMap = true;
-    // let isTable = false;
     return (
       <div>
-      <p>Choose a map of recent cases by country or search for recent cases by city</p>
-      <div>
+        <p>Choose a map of recent cases by country or search for recent cases by city</p>
         <button onClick={this.onClickMap}>Map</button>
-      </div>
-      <div>
         <button onClick={this.onClickTable}>Search by City</button>
-      </div>
-      {this.state.showMap && <Map CountryData={this.state.Data}/>}
-      {this.state.showTable && this.displayTable()}
+        {this.state.showMap && <Map CountryData={this.state.Data}/>}
+        {this.state.showTable && this.displayTable()}
       </div>
     )
   }
