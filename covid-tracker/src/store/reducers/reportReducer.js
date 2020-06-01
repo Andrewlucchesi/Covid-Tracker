897b98d2e80d5d6eaa02b438e8190cae031e522e
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-01 14:41:11 -0700

    Update reportReducer.js

--- covid-tracker/src/store/reducers/reportReducer.js
+++ covid-tracker/src/store/reducers/reportReducer.js
@@ -1,19 +1,18 @@
 const initState ={
-    reports: [
-    ]
+    reports: []
 }
 
 const reportReducer = (state = initState, action) => {
-    switch (action.type){
-        case 'SUBMIT_REPORT':
-            console.log('submitted report', action.project)
-            return state;
-        case 'SUBMIT_REPORT_ERROR':
-            console.log('submit report error', action.err);
-            return state;
-        default:
-            return state;
-    }
+  switch (action.type){
+    case 'SUBMIT_REPORT':
+      console.log('submitted report', action.project)
+      return state;
+    case 'SUBMIT_REPORT_ERROR':
+      console.log('submit report error', action.err);
+      return state;
+    default:
+      return state;
+  }
 }
 
 export default reportReducer 
