8e771d65c60f8970bc691610a1e6655ae139c2f8
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-01 14:41:09 -0700

    Update reportActions.js

--- covid-tracker/src/store/actions/reportActions.js
+++ covid-tracker/src/store/actions/reportActions.js
@@ -1,16 +1,16 @@
 //This file stores actions that can be called by report.js and dispatched to ReportReducer
 //Such as submitting a report
 export const submitReport = (report) => {
-    return (dispatch, getState,{ getFirebase, getFirestore }) => {
-        //make async call to firestore
-        const firestore = getFirestore();
-        firestore.collection('reports').add({
-            ...report, 
-            reportedAt: firestore.FieldValue.serverTimestamp()
-        }).then(()=>{
-            dispatch({  type: 'SUBMIT_REPORT', report});
-        }).catch((err) =>{
-            dispatch({ type: 'SUBMIT_REPORT_ERROR', err});
-        })
-            }
+  return (dispatch, getState,{ getFirebase, getFirestore }) => {
+    //make async call to firestore
+    const firestore = getFirestore();
+    firestore.collection('reports').add({
+      ...report, 
+      reportedAt: firestore.FieldValue.serverTimestamp()
+    }).then(()=>{
+      dispatch({  type: 'SUBMIT_REPORT', report});
+    }).catch((err) =>{
+      dispatch({ type: 'SUBMIT_REPORT_ERROR', err});
+    })
+  }
 };
