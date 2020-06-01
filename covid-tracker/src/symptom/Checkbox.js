2ed1aa55a0d7cc62d248fc3d73b8026405404671
sdyl3 <sandylee0325@g.ucla.edu>
2020-05-31 17:52:26 -0700

    Create Checkbox.js

---
+++ covid-tracker/src/symptom/Checkbox.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react'
+
+class Checkbox extends Component {  
+
+  render() {
+    return (
+      <div>
+        <hr  style={{ color: '#9e9e9e', height: .1 }}/>
+        <h7>Describe your symptom(s):</h7>
+        <p>
+          <label>
+            <input type="checkbox" id="fever" name="fever" onChange={this.handleChange} />
+            <span>Fever</span>
+          </label>
+        </p>
+
+        <p>
+          <label>
+            <input type="checkbox" id="cough" name="cough" onChange={this.handleChange} />
+            <span>Cough</span>
+          </label>
+        </p>
+
+        <p>
+          <label>
+            <input type="checkbox" id="soreThroat" name="soreThroat" onChange={this.handleChange} />
+            <span>Sore Throat</span>
+          </label>
+        </p>
+
+        <p>
+          <label>
+            <input type="checkbox" id="breath" name="breath" onChange={this.handleChange} />
+            <span>Shortness of breath</span>
+          </label>
+        </p>
+
+        <p>
+          <label>
+            <input type="checkbox" id="fatigue" name="fatigue" onChange={this.handleChange} />
+            <span>Fatigue</span>
+          </label>
+        </p>
+
+        <p>
+          <label>
+            <input type="checkbox" id="taste" name="taste" onChange={this.handleChange} />
+            <span>New Loss of taste or smell</span>
+          </label>
+        </p>      
+
+        <p>
+          <label>
+            <input type="checkbox" id="muscle" name="muscle" onChange={this.handleChange} />
+            <span>Muscle Aches</span>
+          </label>
+        </p>
+
+      </div>
+    )
+  }
+}
+
+export default Checkbox 
