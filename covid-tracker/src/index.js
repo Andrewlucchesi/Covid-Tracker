7ee44ecc0ee34100f2414783ec8ff636d9c1c255
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-01 14:40:58 -0700

    Update index.js

--- covid-tracker/src/index.js
+++ covid-tracker/src/index.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
 import { createStore, applyMiddleware, compose } from 'redux'
 import rootReducer from './store/reducers/rootReducer'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import { reduxFirestore, getFirestore, createFirestoreInstance } from 'redux-firestore'
 import { getFirebase, ReactReduxFirebaseProvider } from 'react-redux-firebase'
-import fbConfig from './config/fbconfig'
 import firebase from "firebase/app"
+
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
+import fbConfig from './config/fbconfig'
 
 const store = createStore(rootReducer,
   compose(
