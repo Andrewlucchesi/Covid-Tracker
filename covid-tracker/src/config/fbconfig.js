cf0d1dfca8765ed2bc236511fc3c24dd8ea07967
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-01 14:40:54 -0700

    Update fbconfig.js

--- covid-tracker/src/config/fbconfig.js
+++ covid-tracker/src/config/fbconfig.js
@@ -5,14 +5,14 @@ import 'firebase/analytics'
 
 // Your web app's Firebase configuration
 var firebaseConfig = {
-    apiKey: "AIzaSyDeBT0909rYB9Gt0dNOITRxs00NphiU2F0",
-    authDomain: "ucla-covid-tracker.firebaseapp.com",
-    databaseURL: "https://ucla-covid-tracker.firebaseio.com",
-    projectId: "ucla-covid-tracker",
-    storageBucket: "ucla-covid-tracker.appspot.com",
-    messagingSenderId: "423491376714",
-    appId: "1:423491376714:web:e9a2a5b6bdf94731b478b3",
-    measurementId: "G-FX5R3P4QKN"
+  apiKey: "AIzaSyDeBT0909rYB9Gt0dNOITRxs00NphiU2F0",
+  authDomain: "ucla-covid-tracker.firebaseapp.com",
+  databaseURL: "https://ucla-covid-tracker.firebaseio.com",
+  projectId: "ucla-covid-tracker",
+  storageBucket: "ucla-covid-tracker.appspot.com",
+  messagingSenderId: "423491376714",
+  appId: "1:423491376714:web:e9a2a5b6bdf94731b478b3",
+  measurementId: "G-FX5R3P4QKN"
 };
 
 // Initialize Firebase
