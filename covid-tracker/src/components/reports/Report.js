ca2ff59a6c5f9d25d8e28fc5e5f1417818e3843c
Clement Decker <clement.decker18@gmail.com>
2020-05-31 22:56:25 -0700

    Fixed a bug

--- covid-tracker/src/components/reports/Report.js
+++ covid-tracker/src/components/reports/Report.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { submitReport } from '../../store/actions/reportActions'
 import ReportList from './ReportList' //Report list displays table with report data
 import { firestoreConnect } from 'react-redux-firebase'
 import { compose } from 'redux'
+import { connect } from 'react-redux'
+import { submitReport } from '../../store/actions/reportActions'
 
 class Report extends Component {
 
@@ -27,8 +27,8 @@ class Report extends Component {
             
             <div className="container">
                 <h4 className="center">Report Symptom</h4>
-                <h7> If you or someone in your immediate family has contracted covid-19,
-                    you may use this page to report a diagnosis. </h7>
+                <h5> If you or someone in your immediate family has contracted covid-19,
+                    you may use this page to report a diagnosis. </h5>
                 <p>This will be used to help track cases as they spread through local communities</p>
                 <form onSubmit={ this.handleSubmit }>  
                     <div className="input-field"> 
@@ -40,8 +40,8 @@ class Report extends Component {
                         <input type="number" id="zip" name="zip" onChange={this.handleChange} />
                     </div>
                     <div className="input_field">
-                    <label htmlFor="country">Country</label>
-                        <input type="text" id="country" name="country" onChange={this.handleChange} />
+                    <label htmlFor="country">Country Code</label>
+                        <input type="text" id="Country_Code" name="country" onChange={this.handleChange} />
                     </div>
                     <input type="submit" value="Report Case of Covid-19" />
                 </form>
@@ -63,6 +63,7 @@ const mapDispatchToProps = (dispatch) => {
         submitReport: (report) => dispatch(submitReport(report))
     }
 }
+
 
 //firestoreConnect triggers firebase-state to update when firebase collection changes
 export default compose(connect(mapStateToProps, mapDispatchToProps),
