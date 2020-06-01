9148dde05600bf558668e3d7aebe469a95d760ff
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-01 14:41:00 -0700

    Update Map.js

--- covid-tracker/src/Maps/Map.js
+++ covid-tracker/src/Maps/Map.js
@@ -5,33 +5,30 @@ import countryLoc from './Country'
 const mapStyles = { width: '80%', height: '70%' };
 
 export class MapContainer extends Component {
-
   constructor(props) {
     super(props);
     this.onMarkerClick = this.onMarkerClick.bind(this);
     this.onClose = this.onClose.bind(this);
-    this.state = { countryLoc, 
-                  showingInfoWindow: false,
-                  activeMarker: {},
-                  selectedPlace: "",
-                  countryData: props.CountryData,
-                };
+    this.state = { 
+      countryLoc, 
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: "",
+      countryData: props.CountryData,
+    };
     console.log(this.state.countryData);
   }
 
   onMarkerClick(props, marker, e) {
     let text = "";
     console.log(this.state.countryData);
-    if(this.state.countryData == null)
-    {
+
+    if(this.state.countryData == null){
       text = "Sorry, data is not available right now. Please try later";
     }
-    else
-    {
-      for(const country of this.state.countryData)
-      {
-        if(country.CountryCode === props.country)
-        {
+    else{
+      for(const country of this.state.countryData){
+        if(country.CountryCode === props.country){
           console.log(country.Country);
           text =  "Total Cases: " + country.TotalConfirmed + "\n Total Deaths: " + country.TotalDeaths;
           break;
@@ -39,10 +36,10 @@ export class MapContainer extends Component {
       }
     }
 
-    if(text === "")
-    {
+    if(text === ""){
       text = "No new data";
     }
+
     this.setState({
       selectedPlace: text,
       activeMarker: marker,
@@ -68,25 +65,23 @@ export class MapContainer extends Component {
           position={{ lat: location.latitude, lng: location.longitude }}
           onClick={this.onMarkerClick}
           country={location.countryCode}
-        // onClick={() => console.log("Hello")}
         />,
+        
       ]}).concat(
         <InfoWindow
           onCloseClick={this.onClose}
           marker={this.state.activeMarker}
           visible={this.state.showingInfoWindow}
           key={"InfoWindow"}
-          >
-            <div>
-              <h4>{this.state.selectedPlace}</h4>
-            </div>
-          
-          </InfoWindow> 
+        >
+          <div>
+            <h4>{this.state.selectedPlace}</h4>
+          </div>
+        </InfoWindow> 
       ) 
   }
 
   render() {
-    
     return (
       <Map
         google={this.props.google}
