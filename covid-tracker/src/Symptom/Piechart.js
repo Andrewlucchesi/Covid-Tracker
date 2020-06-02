d6f971a9d66b90a22cb91b02674bd5070713c889
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-02 12:57:01 -0700

    Update Piechart.js

--- covid-tracker/src/Symptom/Piechart.js
+++ covid-tracker/src/Symptom/Piechart.js
@@ -1,62 +1,64 @@
 import React, { Component } from 'react'
 import Pie from 'react-chartjs-2'
 
-class Piechart extends Component {
-  countType(type) {
-    const countTypes = this.props.fevers.filter(
-      fever => fever.media_type === type
-    );
-    return countTypes.length;
-  }
+const data = {
+  labels: [
+    'Fever', 
+    'Cough', 
+    'Sore Throat', 
+    'Shortness of breath', 
+    'Fatigue',
+    'New Loss of taste or smell',
+    'Muscle Aches'
+  ],
 
-  constructor(props){
-    super(props)
-      this.state = {
-        labels: [
-          'Fever', 
-          'Cough', 
-          'Sore Throat', 
-          'Shortness of breath', 
-          'Fatigue',
-          'New Loss of taste or smell',
-          'Muscle Aches'
-        ],
+  datasets: [{
+    data: [1, 3, 5, 2, 4, 3, 2],
+    backgroundColor: [
+      '#374c80', 
+      '#6e5193', 
+      '#9d327f', 
+      '#d85085', 
+      '#ef5350', 
+      '#ff7d42', 
+      '#ffa600'
+    ] 
+  }]
+};
 
-        datasets: [{
-          data: [
-            1, 
-            1, 
-            1, 
-            1, 
-            1, 
-            1, 
-            1
-          ],
-
-          backgroundColor: [
-            '#374c80', 
-            '#6e5193', 
-            '#9d327f', 
-            '#d85085', 
-            '#ef5350', 
-            '#ff7d42', 
-            '#ffa600'
-          ]
-        }]
+const option = {
+  tooltips: {
+    callbacks: {
+      label: function(tooltipItem, data) {
+        var dataset = data.datasets[tooltipItem.datasetIndex];
+        var meta = dataset._meta[Object.keys(dataset._meta)[0]];
+        var total = meta.total;
+        var currentValue = dataset.data[tooltipItem.index];
+        var percentage = parseFloat((currentValue/total*100).toFixed(1));
+        return currentValue + ' (' + percentage + '%)';
+        // return percentage+ '%';
+      },
+      title: function(tooltipItem, data) {
+        return data.labels[tooltipItem[0].index];
       }
+    }
   }
+}
+
+class Piechart extends Component {
 
   render() {
     return (
       <div>
         <hr  style={{ color: '#9e9e9e', height: .1,}}/>
         <h6>Symptom Statistics:</h6>
-        <Pie
+        {/* <Pie
           data={{
             labels: this.state.labels,
             datasets: this.state.datasets
           }}
-        />
+        /> */}
+        <Pie data={data} options={option} />
         <br />
       </div>
     )
