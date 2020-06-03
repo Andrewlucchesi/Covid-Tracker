28994269b59bda9ae852a97677f01f443bcb7059
Clement Decker <clement.decker18@gmail.com>
2020-06-03 12:40:45 -0700

    Fixed bugs

--- covid-tracker/src/components/Newcases.js
+++ covid-tracker/src/components/Newcases.js
@@ -56,24 +56,34 @@ class Newcases extends Component {
 
     if(!this.state.showText){
       return( 
-        <p></p> 
+        <tbody></tbody>
       )
     }
 
     let table = [];
     let cityFound = false;
     let caseCount = 0;
-
+    let lastID = undefined;
     for(const entry of this.state.cases){
       let city = entry.city;
+      if(city == undefined)
+        continue;
       if(((city).toLowerCase()) === (this.state.value).toLowerCase()){
         cityFound = true;
         let children = [];
         let key = entry.id;
-
+        if(key == lastID)
+          {
+            continue;
+            lastID = key;
+          }
+        else
+        {
+          lastID = key;
+        }
         children.push(<td>{entry.city}</td>);
         children.push(<td>{entry.zip}</td>);
-        table.push(<tr key={key}>{children}</tr>);
+        table.push(<tbody><tr key={key}>{children}</tr></tbody>);
         caseCount++;
       }
 
@@ -84,7 +94,15 @@ class Newcases extends Component {
 
     if(!cityFound){
       return(
-        <p>No recently reported cases in that city!</p>
+        <tbody>
+          <tr>
+            <td>
+              No recently reported cases in that city!
+            </td>
+          </tr>
+        </tbody>
+
+        
       )
     }
     else{
