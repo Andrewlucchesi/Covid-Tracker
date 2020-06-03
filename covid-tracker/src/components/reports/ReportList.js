a81e3f5ef54ea43c402005ef87f695ae11d9021a
Clement Decker <clement.decker18@gmail.com>
2020-06-02 23:25:41 -0700

    Removed some unnecessary code

--- covid-tracker/src/components/reports/ReportList.js
+++ covid-tracker/src/components/reports/ReportList.js
@@ -3,7 +3,6 @@ import React from 'react'
 //Report list is called in Report.js to display data. 
 
 const ReportList = ({reports}) => {
-  console.log(reports);
   return (
     <div className="report-list section">
       {reports && reports.map(report => {
