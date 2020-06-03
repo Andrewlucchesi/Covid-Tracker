b31d4516fe1858d0f409042ac29dc92a557531d2
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-03 16:36:43 -0700

    Update Home.js

--- covid-tracker/src/components/Home.js
+++ covid-tracker/src/components/Home.js
@@ -16,15 +16,6 @@ class Home extends Component {
     return (
       <div className="container">
         <h4 className="center">Search for Total Cases by Country</h4>
-            
-          {/* <form onSubmit={ this.handleSubmit } >  
-            <div className="input-field"> 
-              <label htmlFor="country" style={{ marginBottom: 100}} >Enter a country</label>
-              <input type="text" />
-            </div>
-            <input type="submit" value="Search for total cases" />
-          </form>     */}
-
         <Link to="/" style={{ marginRight: 10 }}></Link>
         <Map CountryData={this.state.CountryData} />
       </div>
