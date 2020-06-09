16a6b33d30aaf18cbcfcc066e412e85030e6649e
sdyl3 <sandylee0325@g.ucla.edu>
2020-06-09 14:31:27 -0700

    Update index.js

--- covid-tracker/src/api/index.js
+++ covid-tracker/src/api/index.js
@@ -1,21 +1,12 @@
 import axios from 'axios';
 
 const worldUrl = 'https://api.covid19api.com/summary';
-const countyUrl = 'https://covid19-us-api.herokuapp.com/county';
 const testingLocations = 'https://raw.githubusercontent.com/covid-19-testing/locations/master/california/complete.json';
 
 export const fetchTestingLocs = async () => {
   try {
     const data = await axios.get(testingLocations);
     return data;
-  } catch (error) {}
-}
-
-export const fetchUSACountyData = async () => {
-  try {
-    const { data: {message}} = await axios.get(countyUrl);
-    console.log(message);
-    return message;
   } catch (error) {}
 }
 
